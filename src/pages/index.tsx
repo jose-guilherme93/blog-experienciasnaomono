e7fb9b5d10162b10a13a9bc5434f52b969500e9e
jose-guilherme93 <jose-guilherme93@hotmail.com>
2023-01-14 19:07:45 -0300

    style: added header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
-
+import Head from '@/infra/components/Head'
+import Header from './components/Header'
 
 
 export default function Home() {
+
+  const title = 'o que eu quiser bebezão!'
+
   return (
     <>
-      <h1>firts version</h1>
+        <Head title={title} />
+        <Header />
+        <div className='text-slate-50 underline'>hello</div>
     </>
   )
 }
